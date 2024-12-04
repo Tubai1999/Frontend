873c354b5ddab9d896530fb8b5fb2fc1e56f8244
Tridib Samanta <tridibsamanta311@gmail.com>
2024-12-04 22:36:32 +0530

    menu card

---
+++ food_menu/src/component/Menu.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import styles from "../style/Menu.module.css";
+
+function Menu() {
+  return (
+    <>
+      <div className={styles.maindiv}>
+        <h1 className={styles.header}>TEA COFFE & MORE</h1>
+        <div>
+          <h3>Kadak Chao Ki Piyali Hoo</h3>
+          <span className={styles.rates}> 85</span>
+          <p className={styles.paragraphstyle}>Espresso</p>
+        </div>
+        <div>
+          <h3>Kadak Chao Ki Piyali Hoo</h3>
+          <span className={styles.rates}> 85</span>
+          <p className={styles.paragraphstyle}>Espresso</p>
+        </div>
+        <div>
+          <h3>Kadak Chao Ki Piyali Hoo</h3>
+          <span className={styles.rates}> 85</span>
+          <p className={styles.paragraphstyle}>Espresso</p>
+        </div>
+        <div>
+          <h3>Kadak Chao Ki Piyali Hoo</h3>
+          <span className={styles.rates}> 85</span>
+          <p className={styles.paragraphstyle}>Espresso</p>
+        </div>
+        <div>
+          <h3>Kadak Chao Ki Piyali Hoo</h3>
+          <span className={styles.rates}> 85</span>
+          <p className={styles.paragraphstyle}>Espresso</p>
+        </div>
+        <div>
+          <h3>Kadak Chao Ki Piyali Hoo</h3>
+          <span className={styles.rates}> 85</span>
+          <p className={styles.paragraphstyle}>Espresso</p>
+        </div>
+        <div>
+          <h3>Kadak Chao Ki Piyali Hoo</h3>
+          <span className={styles.rates}> 85</span>
+          <p className={styles.paragraphstyle}>Espresso</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Menu;
